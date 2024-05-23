95f6748743ed6be959a91e8eb098c99364ec23e4
Jason Tralli <jtrallii@live.com>
2024-05-23 17:43:55 -0300

    Navegação - Começando a adicionar os links na navegação

--- frontend/src/components/nav/Navegacao.tsx
+++ frontend/src/components/nav/Navegacao.tsx
@@ -1,13 +1,35 @@
-import React from "react";
-
+import React, { useState } from "react";
 import styles from "./navegacao.module.css";
+import { useFetchFilmes } from "hooks/useEffetsGetDados";
+import { Link } from "react-router-dom";
 
 export default function Navegacao() {
+  
+  const [categoria, setCategoria] = useState("Ação");
+
+  const endpoint = `/filmes/categoria/${
+    categoria === "acao" ? "Ação" :
+    categoria === "aventura" ? "Aventura" :
+    categoria === "comedia" ? "Comedia" :
+    categoria === "crime" ? "Crime" :
+    categoria === "drama" ? "Drama" :
+    "Terror"}`;
+
+  const filmesCategoria = useFetchFilmes({ endpoint: `/filmes/categoria/${endpoint}` });
+
+  const handleCategoriaClick: React.MouseEventHandler<HTMLLIElement> = (event) => {
+    const valor = event.currentTarget;
+    if (valor instanceof HTMLLIElement) {
+        const categoriaEscolhida = valor.id;
+        setCategoria(categoriaEscolhida);
+    }
+};
+
   return (
     <div className={`${styles.mainContainer} ${styles.alinhamento}`}>
       <nav className={styles.alinhamento}>
         <ul className={`${styles.listaStyle} ${styles.alinhamento}`}>
-          <li>AÇÃO</li>
+          <Link to={"filmes/categoria"}><li id="acao" onClick={handleCategoriaClick}>AÇÃO</li></Link>
           <li>ANIMAÇÃO</li>
           <li>SUSPENSE</li>
           <li>TERROR</li>
